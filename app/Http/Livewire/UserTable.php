713fccb2ab05f439481fc645f96bbfd63063cb73
Gerardo Belot <gbelot2003@hotmail.com>
2022-04-17 13:31:23 -0600

    :construction: agregando responsive a tabla de usuarios

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -9,8 +10,51 @@ class UserTable extends Component
 {
     use WithPagination;
 
+    public $showEditModal = false;
+    public $search;
+    public User $editing;
+    public $password;
+    public $rol;
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+        /**
+     * Rules Validation
+     *
+     * @var array
+     */
+    protected $rules = [
+        'editing.name' => 'required',
+        'editing.email' => 'required|email',
+        'editing.password' => 'string',
+        'editing.company_id' => 'required',
+        'editing.settlement_id' => 'required',
+        'editing.status' => 'required',
+        'rol' => 'required',
+    ];
+
+
+
     public function render()
     {
-        return view('livewire.user-table');
+        $rows = User::orderBy('id', 'DESC')
+        ->where('name', 'like', '%' . $this->search . '%')
+        ->orWhere('email', 'like', '%' . $this->search . '%')
+        /**->orWherehas('roles', function($q) {
+            $q->where('name', 'like', '%' . $this->search . '%');
+        })
+        ->orWherehas('company', function($q) {
+            $q->where('name', 'like', '%' . $this->search . '%');
+        })
+        ->orWherehas('settlement', function($q) {
+            $q->where('name', 'like', '%' . $this->search . '%');
+        })*/
+        //->with('roles')
+        ->paginate(10);
+
+        return view('livewire.user-table', compact('rows'));
     }
 }
--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -3,16 +3,78 @@
         <div class="w-full pr-5">
             <div class="flex flex-col md:flex-row bg-white rounded-full" x-data="{ search: '' }">
                 <div class="xl:w-5/6 sm:w-full my-2">
-                    <input type="search"
-                        class="w-full text-gray-900 rounded-full focus:outline-none"
-                        placeholder="Buscar"
-                    >
+                    <input type="search" class="w-full h-9 text-gray-900 rounded-full focus:outline-none"
+                        placeholder="Buscar" x-model="search" wire:model="search">
                 </div>
-                <div class="xl:w-1/6 sm:w-full my-2 pl-2">
-                    <button class="w-full h-9 bg-blue-800  text-white rounded-full hover:bg-indigo-900" wire:click="create">
+                <div class="xl:w-1/6 sm:w-full my-2 xl:pl-2">
+                    <button class="w-full h-9 bg-blue-800  text-white rounded-full hover:bg-indigo-900"
+                        wire:click="create">
                         <i class="fas fa-plus-circle"></i> {{ __('Nuevo Usuario') }}
                     </button>
                 </div>
             </div>
+            <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+                <div class="w-full overflow-x-auto">
+                    <table
+                        class="w-full flex flex-row flex-noa-wrp sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+                        <thead class="text-gray-800">
+                            <tr
+                                class="bg-gray-300 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                                <th class="p-3 text-left">{{ __('Id') }}</th>
+                                <th class="p-3 text-left">{{ __('Nombre') }}</th>
+                                <th class="p-3 text-left">{{ __('Email') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody class="flex-1 sm:flex-none">
+                            @forelse ($rows as $row)
+                                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                                    <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $row->id }}</td>
+                                    <td class="border-grey-light border hover:bg-gray-100 p-3">
+                                        <button class="text-blue-500 hover:text-blue-100"
+                                            wire:click="editModal({{ $row->id }})">
+                                            {{ $row->name }}
+                                        </button>
+                                    </td>
+                                    <td class="border-grey-light border hover:bg-gray-100 p-3">{{ $row->email }}</td>
+                                </tr>
+                            @empty
+                                <tr class="text-gray-700">
+                                    <td class="px-4 py-3 border font-semibold" colspan="7">
+                                        <div class="text-center">
+                                            <strong>{{ __('No data found') }}</strong>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
+
+        <style>
+            @media (min-width: 640px) {
+                table {
+                    display: inline-table !important;
+                }
+
+                thead tr:not(:first-child) {
+                    display: none;
+                }
+            }
+
+            td:not(:last-child) {
+                border-bottom: 0;
+            }
+
+            th:not(:last-child) {
+                border-bottom: 2px solid rgba(0, 0, 0, .1);
+            }
+
+        </style>
+
+
+
+
+
 </section>
